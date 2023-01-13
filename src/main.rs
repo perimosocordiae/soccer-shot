47beb7210ae9ab9f323aea442f70e2745c01b4af
CJ Carey <perimosocordiae@gmail.com>
2023-01-12 23:29:34 -0500

    Allow changing game position

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
-use mouse_rs::{types::keys::Keys, Mouse};
+use clap::Parser;
+use mouse_rs::{
+    types::{keys::Keys, Point},
+    Mouse,
+};
 use screenshots::Screen;
 use std::{fs, thread, time::Duration};
 use xcb::x::{Drawable, GetImage, ImageFormat};
 
 const GAME_WIDTH: u32 = 400;
 const GAME_HEIGHT: u32 = 720;
-const GAME_X: i32 = 550;
-const GAME_Y: i32 = 609;
 const TARGET_RADIUS: i32 = 80;
 const TARGET_DIAMETER: u32 = (TARGET_RADIUS * 2) as u32;
 
+#[derive(Parser)]
+struct Args {
+    #[clap(long, default_value_t = 550)]
+    game_x: i32,
+    #[clap(long, default_value_t = 609)]
+    game_y: i32,
+    #[arg(value_enum, default_value = "none")]
+    shot_type: ShotType,
+}
+
+#[derive(clap::ValueEnum, Debug, Clone)]
+enum ShotType {
+    None,
+    Center,
+    Lob,
+    Manual,
+}
+
 fn main() {
-    let screen = Screen::from_point(GAME_X, GAME_Y).unwrap();
-    println!("Screen: {:?}", screen);
+    let args = Args::parse();
+
+    let screen = Screen::from_point(args.game_x, args.game_y).unwrap();
+    println!("{:?}", screen);
     if screen.display_info.scale_factor != 1.0 {
         panic!("Pixel scaling not yet supported.");
     }
@@ -21,23 +43,48 @@ fn main() {
     }
 
     let image = screen
-        .capture_area(GAME_X, GAME_Y, GAME_WIDTH, GAME_HEIGHT)
+        .capture_area(args.game_x, args.game_y, GAME_WIDTH, GAME_HEIGHT)
         .unwrap();
     fs::write("target/game.png", image.buffer()).unwrap();
 
     let mouse = Mouse::new();
-    let pos = mouse.get_position().unwrap();
-    println!("mouse pos = {:?}", pos);
+    let orig_pos = mouse.get_position().unwrap();
+    println!("mouse pos = {:?}", orig_pos);
     // Restore window focus.
-    mouse.move_to(pos.x, GAME_Y - 5).unwrap();
+    mouse.move_to(orig_pos.x, args.game_y - 5).unwrap();
     mouse.click(&Keys::LEFT).expect("Unable to click LMB");
     thread::sleep(Duration::from_millis(100));
 
-    // Return to original position and start the shot.
-    mouse.move_to(pos.x, pos.y).unwrap();
-    mouse.press(&Keys::LEFT).expect("Unable to press LMB");
+    match args.shot_type {
+        ShotType::None => {
+            return;
+        }
+        ShotType::Manual => {
+            // Return to original position.
+            mouse.move_to(orig_pos.x, orig_pos.y).unwrap();
+        }
+        ShotType::Center => {
+            let x = args.game_x + (GAME_WIDTH / 2) as i32;
+            let y = args.game_y + (11 * GAME_HEIGHT / 16) as i32;
+            mouse.move_to(x, y).unwrap();
+        }
+        ShotType::Lob => {
+            let x = args.game_x + (GAME_WIDTH / 2) as i32;
+            let y = args.game_y + GAME_HEIGHT as i32 - TARGET_RADIUS - 1;
+            mouse.move_to(x, y).unwrap();
+        }
+    }
 
-    // Track the target.
+    // Take the shot.
+    mouse.press(&Keys::LEFT).expect("Unable to press LMB");
+    track_target(mouse.get_position().unwrap());
+    mouse.release(&Keys::LEFT).expect("Unable to release LMB");
+
+    // Restore the original mouse position.
+    mouse.move_to(orig_pos.x, orig_pos.y).unwrap();
+}
+
+fn track_target(pos: Point) {
     let mut prev_red = 0;
     for i in 0..5000 {
         let bgra_pixels = capture(
@@ -50,16 +97,17 @@ fn main() {
         // Count red pixels
         let num_red = bgra_pixels
             .chunks_exact(4)
-            .filter(|bgra| bgra[2] > 200 && bgra[1] < 100 && bgra[0] < 100)
+            .filter(|bgra| bgra[2].saturating_sub(bgra[1]).saturating_sub(bgra[0]) > 0)
             .count();
-        if num_red != prev_red {
+        if num_red == 0 {
+            continue;
+        } else if num_red != prev_red {
             println!("target {}: num red = {}", i, num_red);
             prev_red = num_red;
-        } else if num_red >= 5500 {
+        } else if num_red >= 6200 {
             break;
         }
     }
-    mouse.release(&Keys::LEFT).expect("Unable to release LMB");
 }
 
 fn capture(x: i32, y: i32, width: u32, height: u32) -> Option<Vec<u8>> {
